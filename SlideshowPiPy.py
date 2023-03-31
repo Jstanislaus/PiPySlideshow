43661ef77782f6211c24926fb9dfb41bec4c4849
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-31 17:37:12 +0100

    Update SlideshowPiPy.py

--- SlideshowPiPy.py
+++ SlideshowPiPy.py
@@ -5,13 +5,13 @@ import random
 path = "/home/pi64/PiPySlideshow"
 def show_image(image_path):	
     screen.fill(pygame.Color("white")) # clear the screen	
-        img = pygame.image.load(image_path) # load the image
-        img = img.convert()	
+    img = pygame.image.load(image_path) # load the image
+    img = img.convert()	
     infoObject = pygame.display.Info()
-        set_demensions(img.get_width(), img.get_height()) # set pixel dimensions based on image	
-        x = (infoObject.current_w / 2) - (img.get_width() / 2)
-        y = (infoObject.current_h / 2) - (img.get_height() / 2)
-        screen.blit(img,(x,y))
+    set_demensions(img.get_width(), img.get_height()) # set pixel dimensions based on image	
+    x = (infoObject.current_w / 2) - (img.get_width() / 2)
+    y = (infoObject.current_h / 2) - (img.get_height() / 2)
+    screen.blit(img,(x,y))
     pygame.display.flip()
 piclist =[]
 def updatepics(path,piclist):
