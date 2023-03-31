0db78cfe1f313c8dd81e3eea5e2cf308c985e4e3
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-31 17:46:37 +0100

    Update SlideshowPiPy.py

--- SlideshowPiPy.py
+++ SlideshowPiPy.py
@@ -7,6 +7,36 @@ pygame.init()
 path = "/home/pi64/PiPySlideshow"
 infoObject = pygame.display.Info()
 screen = pygame.display.set_mode((infoObject.current_w,infoObject.current_h), pygame.FULLSCREEN)  # Full screen 
+def set_demensions(img_w, img_h):
+    # Note this only works when in booting in desktop mode. 
+	# When running in terminal, the size is not correct (it displays small). Why?
+
+    # connect to global vars
+    global transform_y, transform_x, offset_y, offset_x
+
+    # based on output screen resolution, calculate how to display
+    ratio_h = (infoObject.current_w * img_h) / img_w 
+
+    if (ratio_h < infoObject.current_h):
+        #Use horizontal black bars
+        #print "horizontal black bars"
+        transform_y = ratio_h
+        transform_x = infoObject.current_w
+        offset_y = (infoObject.current_h - ratio_h) / 2
+        offset_x = 0
+    elif (ratio_h > infoObject.current_h):
+        #Use vertical black bars
+        #print "vertical black bars"
+        transform_x = (infoObject.current_h * img_w) / img_h
+        transform_y = infoObject.current_h
+        offset_x = (infoObject.current_w - transform_x) / 2
+        offset_y = 0
+    else:
+        #No need for black bars as photo ratio equals screen ratio
+        #print "no black bars"
+        transform_x = infoObject.current_w
+        transform_y = infoObject.current_h
+        offset_y = offset_x = 0
 def show_image(image_path):	
     screen.fill(pygame.Color("white")) # clear the screen	
     img = pygame.image.load(image_path) # load the image
