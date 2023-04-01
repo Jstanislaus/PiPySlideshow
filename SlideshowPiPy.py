ec170421e0cbe7b868adcbbcf15d5576ee4dd789
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-04-01 11:41:48 +0100

    changed ip

--- SlideshowPiPy.py
+++ SlideshowPiPy.py
@@ -100,7 +100,7 @@ while True:
     if i ==count:
         i=0
     if j%5==3:
-        gpout = subprocess.Popen("rsync -avz -e ssh pi@192.168.1.212:Photobooth-printer-management/Photobooth_printer_management/Template/AH_2023_04_01_Phils_40th/Final_Templates/ PiPySlideshow",shell =True) 
+        gpout = subprocess.Popen("rsync -avz -e ssh pi@192.168.1.249:Photobooth-printer-management/Photobooth_printer_management/Template/AH_2023_04_01_Phils_40th/Final_Templates/ PiPySlideshow",shell =True) 
         gpout1=gpout.wait()
         piclist = updatepics(path,piclist)
         random.shuffle(piclist)
